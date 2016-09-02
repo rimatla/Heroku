2de36aa0da26f5cf5830805f1eaaab19790e1827
Altamir Coelho <alt.coelho@gmail.com>
2016-09-02 17:08:39 -0500

    add coment to server.js

--- server.js
+++ server.js
@@ -4,10 +4,11 @@
 
 var http = require('http');
 
-http.createServer(function(request, response){
-    response.writeHead(200);
-    response.write('Let\'s see if Heroku wants to be my friend');
-    response.end();
-}).listen(process.env.PORT || 3000);
+    //set up server
+    http.createServer(function(request, response){
+        response.writeHead(200);
+        response.write('Let\'s see if Heroku wants to be my friend');
+        response.end();
+    }).listen(process.env.PORT || 3000);
 
-console.log('Running on port 3000');
+    console.log('Running on port 3000');
